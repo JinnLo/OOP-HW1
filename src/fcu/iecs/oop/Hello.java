c4ebbe0933d3160c0fc051265f1447357bff1d39
user <user@114.46.37.173>
2016-10-03 00:47:05 +0800

    change lyrics "it" to "her" and "Cannot" to "Can't"

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -4,7 +4,13 @@ public class Hello {
 	
 	public static void main (String[] args)
 	{
-		String lyrics ="Let her go! Let her go!Can't hold it back anymore";
+		String lyrics ="Let it go! Let it go!Cannot hold it back anymore";
+		
+		lyrics = lyrics.substring(0,3).concat(" ")  + lyrics.substring(4,6).replace('i', 'h').replace('t', 'e').concat("r")
+				+ lyrics.substring(6,10) + lyrics.substring(11,14).concat(" ")  
+				+ lyrics.substring(15,17).replace('i', 'h').replace('t', 'e').concat("r")
+				+ lyrics.substring(17,21) + lyrics.substring(21,24)
+				+ lyrics.substring(25,27).replace('o', '\'').concat(" ")+lyrics.substring(27,48); 
 		System.out.println(lyrics);
 	}
 }
